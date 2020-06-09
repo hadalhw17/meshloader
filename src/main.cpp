d2596e6f6d43674a17ca6d302e34e74b9a503591
hadalhw17 <gerons.y@gmail.com>
2020-06-09 23:25:27 +0300

    Add vulkan detection Add GLM conan package
    - Vulkan option is now being added to the CMake file
    
    - GLM option is not in the CMake and it is loaded using conan

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <spdlog/spdlog.h>
 #include <fmt/printf.h>
 #include "imgui.h"
-#include "examples/imgui_impl_glfw.h"
-#include "examples/imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <vulkan/vulkan.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 #include <cassert>
 
 
 int main()
 {
+    VkApplicationInfo appInfo{VK_STRUCTURE_TYPE_APPLICATION_INFO};
+    appInfo.apiVersion = VK_API_VERSION_1_1;
+    appInfo.applicationVersion = 0.1;
+    appInfo.engineVersion = 0.1;
+    appInfo.pApplicationName = "DemoApp";
+    appInfo.pEngineName = "DemoEngine";
+
+    VkInstanceCreateInfo instanceInfo{VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkInstance instance;
+    VkResult res = vkCreateInstance(&instanceInfo, nullptr, &instance);
+    if(res != VK_SUCCESS)
+    {
+        spdlog::error("Vulkan failed to initialise(libraries not loaded)");
+        return 1;
+    }
     if(!glfwInit())
     {
         return 1;
@@ -20,7 +39,6 @@ int main()
     {
         return 1;
     }
-
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
@@ -34,6 +52,10 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
+    glm::vec3 test_a = glm::vec3(10.f, 0.f, 1.f);
+    glm::vec3 test_b = glm::vec3(13.f, 1.f, 55.f);
+    glm::vec3 res_glm = test_a + test_b;
+    spdlog::info("glm test: (10.f, 0.f, 1.f)+(13.f, 1.f, 55.f) = ({}, {}, {})", res_glm.x, res_glm.y, res_glm.z);
     spdlog::info("Hello {}!", "World");
     fmt::print("Hello {} from fmt", "World!");
 
@@ -58,5 +80,6 @@ int main()
     ImGui::DestroyContext();
     glfwDestroyWindow(window);
     glfwTerminate();
+    vkDestroyInstance(instance, nullptr);
     return 0;
 }
