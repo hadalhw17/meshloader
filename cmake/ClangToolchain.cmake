ffba029eb63f3d8c5131e4f1d59a31fd2ff11884
hadalhw17 <gerons.y@gmail.com>
2020-06-11 14:21:18 +0300

    Add tests and travis CI support
    - Fuzz testing with clang fuzzer
    - Unit tests with ctest
    - CI for coverage

---
+++ cmake/ClangToolchain.cmake
@@ -0,0 +1,7 @@
+    SET (CMAKE_CXX_COMPILER     "/usr/bin/clang++")
+
+    SET (CMAKE_AR      "/usr/bin/llvm-ar")
+    SET (CMAKE_LINKER  "/usr/bin/llvm-ld")
+    SET (CMAKE_NM      "/usr/bin/llvm-nm")
+    SET (CMAKE_OBJDUMP "/usr/bin/llvm-objdump")
+    SET (CMAKE_RANLIB  "/usr/bin/llvm-ranlib")
